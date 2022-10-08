752949fd10b1386f0f4fba196c09086faaead6b5
farhoud <farhoud.m7@gmail.com>
2022-10-08 19:03:14 +0330

    Add Core Entities Entities

---
+++ entity/entites.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"time"
+)
+
+type Status int
+
+const (
+    Pending Status = iota
+    Sent
+    Seen
+)
+
+type Message struct {
+	ID            string
+	CreatedAt     time.Time
+	Text          string
+	Status        Status
+	Author        Contact
+}
+
+type Contact struct {
+	ID string
+	Name string
+}
+
+type ChatInfo struct {
+	ID string
+	Name string
+	Members []Contact
+}
+
+type Chat struct {
+	Info ChatInfo
+	Messages []Message
+}
+
