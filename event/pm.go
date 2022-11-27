dcfe276a55307eb6fa5045be2c3284dadf0fea66
farhoud <farhoud.m7@gmail.com>
2022-11-27 18:28:52 +0330

    remove used code

--- event/pm.go
+++ event/pm.go
@@ -1,14 +1,8 @@
 package event
 
 import (
-	"github.com/hood-chat/core/entity"
 	"github.com/hood-chat/core/pb"
 )
-
-type EvtMessageStatusChange struct {
-	ID     entity.ID
-	Status entity.Status
-}
 
 type EvtMessageReceived struct {
 	Msg *pb.Message
